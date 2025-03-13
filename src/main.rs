ee06c2631b89202f5f1bb0ccb5e20f0453269476
Juan A <j.alejandrominaya@gmail.com>
2025-03-13 23:04:17 +0100

    test: Expected pairing up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn main() {}
+
+pub fn pair_up(participants: HashMap<char, HashSet<char>>) -> HashSet<(char, char)> {
+    HashSet::new()
+}
 
 struct Party<'a> {
     preferences: &'a str,
@@ -17,21 +23,41 @@ impl Party<'_> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::{HashMap, HashSet};
+
     use super::*;
     const ALINA: Party = Party { preferences: "BC" };
 
     #[test]
-    fn test_can_return_other_parties_interested() {
-        /* For the purposes of this test
+    fn test_pairing_up_returns_stable_pairing() {
+        /*
+         * For the purposes of this test
          * Group #1: A(lina), D(olores)
          * Group #2: B(eto), C(harlie)
+         *
+         * Rankings:
+         * A = ["C", "B"]
+         * B = ["A", "D"]
+         * D = ["B", "C"]
+         * C = ["D", "A"]
+         *
+         * Proposers: Group #2
+         *
+         * Expected:
+         * (C, A), (B, D)
+         *
          */
-        assert_eq!(ALINA.how_interested_in('B'), 1);
-        assert_eq!(ALINA.how_interested_in('C'), 2);
-    }
+        let participants: HashMap<char, HashSet<char>> = HashMap::from([
+            ('A', HashSet::from(['C', 'B'])),
+            ('B', HashSet::from(['A', 'D'])),
+            ('C', HashSet::from(['D', 'A'])),
+            ('D', HashSet::from(['B', 'C'])),
+        ]);
 
-    #[test]
-    fn test_returns_the_maximum_for_unknown_parties() {
-        assert_eq!(ALINA.how_interested_in('D'), usize::MAX);
+        let expected = HashSet::from([('C', 'A'), ('B', 'D')]);
+
+        let actual = pair_up(participants);
+
+        assert_eq!(actual, expected);
     }
 }
